a9eeee75a7f9535bb69175c24ed133fb2f6a9d70
galimba <github@galimba.com.ar>
2019-01-16 01:10:51 -0300

    initial commit

---
+++ Player.hpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <assert.h>
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavdevice/avdevice.h>
+#include <libavfilter/avfilter.h>
+#include <libavformat/avformat.h>
+#include <libavformat/avio.h>
+#include <libavutil/avutil.h>
+//#include <libpostproc/postprocess.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+#include <libavutil/frame.h>
+#include <libavutil/imgutils.h>
+}
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_thread.h"
+#include "SDL2/SDL_syswm.h"
+#include "SDL2/SDL_render.h"
+#include "SDL2/SDL_audio.h"
+
+#define SDL_MAIN_HANDLED
+#define ERROR_SIZE 128
+#define FORMATO AV_PIX_FMT_RGB24
+#define SDL_AUDIO_BUFFER_SIZE 1024;
+#define AVCODEC_MAX_AUDIO_FRAME_SIZE 192000
+
+typedef struct _AudioPacket
+	{
+		AVPacketList *first, *last;
+		int nb_packets, size;
+  		SDL_mutex *mutex;
+  		SDL_cond *cond;
+	} AudioPacket;
+
+class Player {
+
+public:
+	
+	float start_w = 0;
+	float end_w = 1;
+	float start_h = 0;
+	float end_h = 1;
+
+	std::string filename;
+
+	bool video_output;
+
+	Player(std::string afilename, float astart_w, float aend_w, float astart_h, float aend_h, bool avo) : video_output(avo), start_w(astart_w), end_w(aend_w), start_h(astart_h), end_h(aend_h) {
+
+		audioStream = -1;
+
+		//init ffmpeg
+		av_register_all();
+
+		filename = afilename;
+
+		//open video
+		int res = avformat_open_input(&pFormatCtx, filename.c_str(), NULL, NULL);
+
+		//check video opened
+		if (res!=0){
+			show_error(res);
+			exit(-1);
+		}
+
+		//get video info
+		res = avformat_find_stream_info(pFormatCtx, NULL);
+		if (res < 0) {
+			show_error(res);
+			exit(-1);
+		}
+
+		//get video stream
+		videoStream = get_parameters();
+		if (videoStream == -1) {
+			std::cout << "Error opening your video using AVCodecParameters, does not have codecpar_type type AVMEDIA_TYPE_VIDEO" << std::endl;
+			exit(-1);
+		}
+
+		if (lerCodecVideo() < 0) exit(-1);
+
+	}
+
+	~Player(void) {
+
+		av_free(buffer);
+		av_free(pFrameRGB);
+
+		// Free the YUV frame
+		av_free(pFrame);
+
+		// Close the codecs
+		avcodec_close(pCodecCtx);
+
+		// Close the video file
+		avformat_close_input(&pFormatCtx);
+
+	}
+
+	
+	void video_info(void);
+	int alloc_memory(void);
+	int lerFramesVideo(void);
+	int create_window(void);
+	
+	static int getAudioPacket(AudioPacket*, AVPacket*, int);
+
+private:
+	
+	void memsetAudioPacket(AudioPacket * pq);
+	int videoStream;
+
+	int audioStream;
+
+	AVFormatContext *pFormatCtx = NULL;
+
+	AVCodecParameters *pCodecParameters = NULL;
+
+	AVCodecParameters *pCodecAudioParameters = NULL;
+
+	AVCodecContext *pCodecCtx = NULL;
+
+	AVCodecContext *pCodecAudioCtx = NULL;
+
+	SDL_AudioSpec wantedSpec = { 0 }, audioSpec = { 0 };
+
+	AVCodec *pCodec = NULL;
+
+	AVCodec *pAudioCodec = NULL;
+
+	AVFrame *pFrame = NULL;
+	AVFrame *prevFrame = NULL;
+	uint8_t *tmpFrame = NULL;
+	AVFrame *pFrameRGB = NULL;
+
+	uint8_t *buffer = NULL;
+
+	struct SwsContext *sws_ctx = NULL;
+
+	SDL_Window *screen;
+
+	SDL_Renderer *renderer;
+
+	SDL_Texture* bmp;
+	
+	void show_error(int erro);
+
+	int get_parameters(void);
+
+	int lerCodecVideo(void);
+
+	
+	void initAudioPacket(AudioPacket *); 
+
+	int putAudioPacket(AudioPacket *, AVPacket *); 
+
+};
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include "Player.hpp"
+#include <string>
+#include <sstream>
+
+#ifdef main
+#undef main
+#endif
+
+using namespace std;
+
+
+bool check_floating_point(std::string astring)
+{
+	std::istringstream iss(astring);
+	float tmp;
+	iss >> noskipws >> tmp;
+	return iss.eof() && !iss.fail();
+}
+
+int main(int argc, const char *argv[]) {
+
+	if(argc != 7){
+		cout << "Usage: ./player filename width_start width_end height_start height_end video_output[0,1]" << endl;
+		exit(-1);
+	}
+	
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+		cout << "There is something wrong with your SDL Libs. Can't run" << endl;
+		exit(-1);
+	}
+
+	//"20180823_134013.MOV"
+	std::string filename = std::string(argv[1]);
+	
+	bool check_sw = check_floating_point(argv[2]);
+	bool check_ew = check_floating_point(argv[3]);
+	bool check_sh = check_floating_point(argv[4]);
+	bool check_eh = check_floating_point(argv[5]);
+
+	bool video_output = stoi(argv[6]);
+
+	if (!check_sw || !check_ew || !check_sh || !check_eh)
+	{
+		cout << "Wrong: Windows dimentions must be between [0..1]" << endl;
+		cout << "Usage: ./player filename width_start width_end height_start height_end video_output" << endl;
+		exit(-1);
+	}
+	
+	float start_w_param = std::atof(argv[2]);
+	float end_w_param = std::atof(argv[3]);
+	float start_h_param = std::atof(argv[4]);
+	float end_h_param = std::atof(argv[5]);
+
+	
+	Player * player = new Player(filename, start_w_param, end_w_param, start_h_param, end_h_param, video_output);
+	
+	int res = player->alloc_memory();
+	if (res < 0) {
+		cout << "Fatal Error";
+		delete(player);
+		exit(-1);
+	}
+
+	res= player->create_window();
+
+	res = player->lerFramesVideo();
+	if (res < 0) {
+		cout << "Shit happens, try again" << endl;
+		delete(player);
+		exit(-1);
+	}
+
+	delete(player);
+
+	return 0;
+}
